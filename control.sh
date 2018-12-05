5958d81028c17a22eb40eca7db6d07e6ac079f80
Ben Healey <bentastic27@gmail.com>
2018-12-04 23:16:03 -0500

    added play to reset to rancher cluster

--- control.sh
+++ control.sh
@@ -14,11 +14,15 @@ elif [ "$1" = "reset" ]; then
 elif [ "$1" = 'kubeadm_cluster' ]; then
     ansible-playbook -i hosts --ask-vault-pass --extra-vars '@passwd.yaml' reset_homelab.yaml install_kubeadm_cluster.yaml
 
+elif [ "$1" = 'reset_rancher' ]; then
+    ansible-playbook -i hosts --ask-vault-pass --extra-vars '@passwd.yaml' reset_rancher.yaml
+
 else
     echo "available commands:"
     echo
     echo "init"
     echo "reset"
     echo "kubeadm_cluster"
+    echo "reset_rancher"
     echo
 fi
